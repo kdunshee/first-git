d7ba246142d3ede9438c183d31fe65c6eb777698
Kyle <kyledunshee@Kyles-MacBook-Pro.local>
2020-05-14 11:23:11 -0600

    added array

---
+++ rando.rb
@@ -0,0 +1,7 @@
+students = ["Leveille, Andre", "AshWorth, Ryan", 
+"Biddinger, Caden", "Mbia, Arsene", 
+"Boyden, Lindsay", "DelaCruz, Joey", 
+"Dunshee, Kyle", "Riches, James", "Smith, Shawn", "
+Sultan, Maxwell", "Zarate, Javier"]
+
+puts students
